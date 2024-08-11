462ef3f46e0d8b2a80011b6656c58be44a5bf616
Aidan Blancas <aidanblncs@gmail.com>
2024-08-10 20:34:19 -0500

    Initial Commit

---
+++ Listen-1.0.0/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Listen-1.0.0
+//
+//  Created by Aidan Blancas on 8/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Listen-1.0.0/Listen_1_0_0App.swift
@@ -0,0 +1,17 @@
+//
+//  Listen_1_0_0App.swift
+//  Listen-1.0.0
+//
+//  Created by Aidan Blancas on 8/10/24.
+//
+
+import SwiftUI
+
+@main
+struct Listen_1_0_0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
