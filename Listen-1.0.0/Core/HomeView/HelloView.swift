d81cdfc5d1373a09d3a990ed8a8f9c75d0df3a76
Aidan Blancas <aidanblncs@gmail.com>
2024-10-04 13:47:01 -0500

    base commit / file structure

---
+++ Listen-1.0.0/Core/HomeView/HelloView.swift
@@ -0,0 +1,18 @@
+//
+//  HelloView.swift
+//  Listen-1.0.0
+//
+//  Created by Aidan Blancas on 10/3/24.
+//
+
+import SwiftUI
+
+struct HelloView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HelloView()
+}
